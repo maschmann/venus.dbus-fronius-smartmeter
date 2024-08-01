6af0612900e7130d712a5eb3da96e26dce7f28b4
Marc Aschmann <marc@aschmann.org>
2024-08-01 12:42:00 +0200

    adjust pylint

--- dbus-fronius-smartmeter/ext/velib_python/ve_utils.py
+++ dbus-fronius-smartmeter/ext/velib_python/ve_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# pylint: skip-file
 # -*- coding: utf-8 -*-
 import sys
 from traceback import print_exc
--- dbus-fronius-smartmeter/ext/velib_python/vedbus.py
+++ dbus-fronius-smartmeter/ext/velib_python/vedbus.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# pylint: skip-file
 # -*- coding: utf-8 -*-
 
 import dbus.service
